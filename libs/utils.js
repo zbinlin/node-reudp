063fa1af3423e0e6dc527147ccd71e52664fee67
Zheng Chaoping <zbinlin@gmail.com>
2016-08-10 01:08:59 +0800

    {FEAT}: add util functions (zipSeqArray, unzipSeqArray)

---
+++ libs/utils.js
@@ -0,0 +1,88 @@
+"use strict";
+
+const utils = module.exports = {};
+
+const asc = (a, b) => Math.sign(a - b);
+//const dsc = (a, b) => Math.sign(b - a);
+const unique = ary => [...new Set(ary).values()];
+
+/**
+ * compress continuous sequence number
+ * @param {number[]} ary
+ * @param {number[]}
+ */
+utils.zipSeqArray = function zipSeqArray(ary) {
+    if (!Array.isArray(ary)) {
+        throw new TypeError("argument 0 must be an array");
+    }
+    const sortedAry = unique(ary).sort(asc);
+    const FLAGS = 0x8000;
+    const zippedAry = [];
+    let start, prev;
+    for (let i = 0, len = sortedAry.length; i < len; i++) {
+        let curr = sortedAry[i];
+        if (curr >= FLAGS || curr < 0) {
+            throw new Error(`value(${curr}) must be greater than or equal to 0 and less than ${FLAGS}`);
+        }
+        if (prev !== undefined) {
+            if (curr === prev) {
+                // ignore
+                continue;
+            } else if (curr - prev === 1) {
+                if (start === undefined) {
+                    start = prev;
+                }
+            } else {
+                if (start !== undefined) {
+                    zippedAry.push(start | FLAGS, prev | FLAGS);
+                    start = undefined;
+                } else {
+                    zippedAry.push(prev);
+                }
+            }
+        }
+        prev = curr;
+    }
+    if (prev !== undefined) {
+        if (start !== undefined) {
+            zippedAry.push(start | FLAGS, prev | FLAGS);
+        } else {
+            zippedAry.push(prev);
+        }
+    }
+    return zippedAry;
+};
+
+/**
+ * decompress continuous sequence number
+ * @param {number[]} ary
+ * @return {number[]}
+ */
+utils.unzipSeqArray = function unzipSeqArray(ary) {
+    if (!Array.isArray(ary)) {
+        throw new TypeError("argument 0 must be an array");
+    }
+    const sortedAry = unique(ary).sort(asc);
+    const MAX_INT16 = 0x7FFF;
+    const unzippedAry = [];
+    for (let i = 0, len = sortedAry.length; i < len; i++) {
+        let curr = sortedAry[i];
+        if (curr > MAX_INT16) {
+            let start = curr & MAX_INT16;
+            i += 1;
+            let next = sortedAry[i];
+
+            if (next > MAX_INT16) {
+                let end = next & MAX_INT16;
+                for (let j = start; j <= end; j++) {
+                    unzippedAry.push(j);
+                }
+            } else {
+                throw new RangeError(`malformed range: [${curr}, ${next}]`);
+            }
+        } else {
+            unzippedAry.push(curr);
+        }
+    }
+    return unique(unzippedAry);
+};
